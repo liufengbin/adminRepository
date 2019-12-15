d068a78ff74a518397d470fc9974ec84d688fbd6
liufengbin <283564061@qq.com>
2019-12-16 02:01:58 +0800

    初始导入

---
+++ main/src/main/java/com/bjpowernode/beans/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.beans;
+
+/**
+ * className:Person
+ * package:com.bjpowernode.beans
+ * Description:TODO
+ *
+ * @date:16/12/201901:59
+ * @author:LiuFengbin
+ */
+public class Person {
+
+}
